4341034bea36d465a00478ec1c77798e107a6313
EduardToth <eduardtoth98@gmail.com>
2020-03-01 12:28:19 +0200

    most tenyleg kesz van

--- Server.cpp
+++ Server.cpp
@@ -6,44 +6,45 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <signal.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <csignal>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 
 void Server::execute() {
-    struct sockaddr_in server = {AF_INET, 8001, INADDR_ANY};
+    struct sockaddr_in server = {AF_INET, PORT, INADDR_ANY};
     set_signals();
     set_socket(server);
 
-    printf("Waiting for Players to join in TicTacToe Club.. \n");
-    strcpy(a[0], "Waiting for the other Player to join\n");
+    printf("Waiting for Players to join\n");
+    strcpy(message_for_client[0], "Waiting for the other Player to join\n");
 
     wait_for_players();
     listen_to_client_calls();
-    close(newsockfd[0]);
-    close(newsockfd[1]);
+    close(client_socket_pids[0]);
+    close(client_socket_pids[1]);
 }
 
 void Server::set_signals(){
-static struct sigaction act;
-
-act.sa_handler = catcher;
-sigfillset(&(act.sa_mask));
-sigaction(SIGPIPE, &act, 0);
-sigaction(SIGTSTP, &act, 0);
-sigaction(SIGINT, &act, 0);
+    struct sigaction action;
+    action.sa_handler = execute_catch_block;
+    sigfillset(&(action.sa_mask));
+    sigaction(SIGPIPE, &action, 0);
+    sigaction(SIGTSTP, &action, 0);
+    sigaction(SIGINT, &action, 0);
 }
 
 void Server::set_socket(struct sockaddr_in & server) {
-    if ((sockfd[0] = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    if ((server_socket_fd[0] = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         perror("Socket Call Failed");
         exit(1);
     }
 
-    if (bind(sockfd[0], (struct sockaddr *)&server, SIZE) == -1)
+    if (bind(server_socket_fd[0], (struct sockaddr *)&server, SIZE) == -1)
     {
         perror("Bind Call Failed");
         exit(1);
@@ -51,21 +52,17 @@ void Server::set_socket(struct sockaddr_in & server) {
 
 }
 
-void Server::catcher(int sig)
-{
+void Server::execute_catch_block(int sig) {
     printf("Quitting\n");
 }
 
-void Server::check(char playBoard[][3])
-{
-    int i;
+char Server::identify_winner(char playBoard[][3]) {
     char key = ' ';
 
-    //prin for-urile astea doua se va verifica daca a castigat vreun jucator
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
         if (playBoard[i][0] == playBoard[i][1] && playBoard[i][0] == playBoard[i][2] && playBoard[i][0] != ' ')
             key = playBoard[i][0];
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
         if (playBoard[0][i] == playBoard[1][i] && playBoard[0][i] == playBoard[2][i] && playBoard[0][i] != ' ')
             key = playBoard[0][i];
     if (playBoard[0][0] == playBoard[1][1] && playBoard[1][1] == playBoard[2][2] && playBoard[1][1] != ' ')
@@ -73,22 +70,26 @@ void Server::check(char playBoard[][3])
     if (playBoard[0][2] == playBoard[1][1] && playBoard[1][1] == playBoard[2][0] && playBoard[1][1] != ' ')
         key = playBoard[1][1];
 
+    return key;
+}
 
+void Server::someone_won(char playBoard[][3]) {
+
+    char key = identify_winner(playBoard);
     if (key == 'X')
     {
 
         printf("Player 1 Wins\n\n");
-        kill(pid[0], SIGUSR1);
-        kill(pid[1], SIGUSR1);
+        kill(client_pids[0], SIGUSR1);
+        kill(client_pids[1], SIGUSR1);
         running = false;
     }
-
     if (key == 'O')
     {
 
         printf("Player 2 Wins\n\n");
-        kill(pid[0], SIGUSR2);
-        kill(pid[1], SIGUSR2);
+        kill(client_pids[0], SIGUSR2);
+        kill(client_pids[1], SIGUSR2);
         running = false;
     }
 }
@@ -110,7 +111,7 @@ void Server::listen_to_client_calls() {
             break;
         }
 
-        read(newsockfd[ctr], serverRead, sizeof(serverRead));
+        read(client_socket_pids[ctr], serverRead, sizeof(serverRead));
         choice = atoi(serverRead);
         printf("Server side the Integer received is: %d\n", choice);
         row = --choice / 3;
@@ -121,44 +122,53 @@ void Server::listen_to_client_calls() {
         else
             ctr = 1;
 
-        write(newsockfd[ctr], playBoard, sizeof(playBoard));
-        check(playBoard);
+        write(client_socket_pids[ctr], playBoard, sizeof(playBoard));
+        someone_won(playBoard);
     }
 }
 
 void Server::wait_for_players() {
-    int usr = 0;
-    char x[4];
-    while (usr < 2)
+    int nr_users = 0;
+
+    while (nr_users < 2)
     {
-        if (listen(sockfd[0], 5) == -1)
+        if (listen(server_socket_fd[0], 5) == -1)
         {
             perror("Listen Call Failed\n");
             exit(1);
         }
 
-        newsockfd[usr] = accept(sockfd[0], 0, 0);
-        usr++;
+        client_socket_pids[nr_users++] = accept(server_socket_fd[0], 0, 0);
 
-        if (usr == 1)
+        if (nr_users == 1)
         {
-            strcpy(a[1], "0");
-            write(newsockfd[0], a, sizeof(a));
-            read(newsockfd[0], x, sizeof(x));
-            pid[0] = atoi(x);
+            join_first_player();
         }
 
-        printf("No. of Players who joined the Club: %d\n", usr);
+        printf("No. of Players who joined the Club: %d\n", nr_users);
 
-        if (usr == 2)
+        if (nr_users == 2)
         {
-            strcpy(a[0], "Let's play Tic Tac Toe!!");
-            strcpy(a[1], "1");
-            write(newsockfd[0], a, sizeof(a));
-            strcpy(a[1], "2");
-            write(newsockfd[1], a, sizeof(a));
-            read(newsockfd[1], x, sizeof(x));
-            pid[1] = atoi(x);
+            join_second_player();
         }
     }
+}
+
+void Server::join_second_player(){
+    char pid_in_string_form[4];
+    strcpy(message_for_client[0], "Game starts!!!");
+    strcpy(message_for_client[1], "1");
+    write(client_socket_pids[0], message_for_client, sizeof(message_for_client));
+    strcpy(message_for_client[1], "2");
+    write(client_socket_pids[1], message_for_client, sizeof(message_for_client));
+    read(client_socket_pids[1], pid_in_string_form, sizeof(pid_in_string_form));
+    client_pids[1] = atoi(pid_in_string_form);
+}
+
+void Server::join_first_player() {
+    char pid_in_string_form[4];
+    strcpy(message_for_client[1], "0");
+    write(client_socket_pids[0], message_for_client, sizeof(message_for_client));
+    read(client_socket_pids[0], pid_in_string_form, sizeof(pid_in_string_form));
+    client_pids[0] = atoi(pid_in_string_form);
 }
--- Server.h
+++ Server.h
@@ -5,22 +5,25 @@
 #ifndef SERVER_SERVER_H
 #define SERVER_SERVER_H
 #include <netinet/in.h>
-
+#define PORT 8001
 
 class Server{
 private:
     const int SIZE = sizeof(struct sockaddr_in);
-    char a[2][40];
-    int sockfd[2];
+    char message_for_client[2][40];
+    int server_socket_fd[2];
     bool running = true;
-    int newsockfd[2];
-    int pid[2];
+    int client_socket_pids[2];
+    int client_pids[2];
     void wait_for_players();
     void listen_to_client_calls();
-    void check(char playBoard[][3]);
-    static void catcher(int sig);
+    void someone_won(char playBoard[][3]);
+    static void execute_catch_block(int sig);
     void set_socket(struct sockaddr_in & server);
     static void set_signals();
+    char identify_winner(char playBoard[][3]);
+    void join_first_player();
+    void join_second_player();
 public:
     void execute();
 };
